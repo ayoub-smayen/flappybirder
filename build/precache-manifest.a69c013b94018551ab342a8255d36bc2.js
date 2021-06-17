99469ceaf47eff9947d0bccaf0dbbee424c49fbb
ayoubsmayen2 <ayoub.smayen@esprit.tn>
2021-06-17 23:29:08 +0200

    ert

---
+++ build/precache-manifest.a69c013b94018551ab342a8255d36bc2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2929d03e55bd7b6d9b83bfbd63e83a99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47366d6235109162799f",
+    "url": "/static/css/main.c802b0fe.chunk.css"
+  },
+  {
+    "revision": "2a9ca907d26e92348842",
+    "url": "/static/js/2.35e57557.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.35e57557.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47366d6235109162799f",
+    "url": "/static/js/main.66c55155.chunk.js"
+  },
+  {
+    "revision": "583d3f582912e42e3447",
+    "url": "/static/js/runtime-main.1ebd6d0e.js"
+  }
+]);
